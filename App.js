65159a6e83a549f71730feb74049da6353dc483e
Adarsha-React <adarshareact2023@gmail.com>
2023-12-31 16:50:55 +0530

    episode-03:React Components & JSX

--- App.js
+++ App.js
@@ -1,39 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent1 = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I'm an h1 tag")
-  )
-);
+const Title = () => {
+  return (
+    <div>
+      <h1>Namaste React🚀</h1>
+    </div>
+  );
+};
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement(
-    "div",
-    { id: "child-1", key: "child1" },
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag")
-  ),
-  React.createElement(
-    "div",
-    { id: "child-1", key: "child2" },
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag")
-  ),
-]);
+//Use JSX (HTML like sytax but this is not HTML inside JavaScript)
+//JSX --> will be transpiled into React.createElement using Babel
 
-//we will use JSX to write the above code in easy way
+//JSX ==> React.createElement ==> HTMLElement
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World From React!!"
-);
+//const jsxHeading = <h1 id="heading">Hello World from JSX🚀</h1>;
 
-console.log(heading);
+//JSX will sanitize the data before using it, it escapes xss attaching
+
+const Header = () => {
+  return (
+    <div>
+      {Title()}
+      <Title />
+      <Title></Title>
+      <h1 id="heading">Hello world from Functional component</h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Header />);
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Namaste React 🙏
+
+## React Components
+
+**Functional Components**
+**Class Based Components**
+
+## Functional Components
+
+- New way of writing the components in React.
+- It is just a normal JavaScript function and it returns some piece of JSX.
+
+## JSX
+
+- Use JSX (HTML like sytax but this is not HTML inside JavaScript)
+- JSX ==> will be transpiled into React.createElement using **_Babel_**
+- JSX ==> React.createElement ==> HTMLElement
+- JSX will sanitize the data before using it, it escapes xss attaching
